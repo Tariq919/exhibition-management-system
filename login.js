f7e20077916586a955340b0681db7e5057a54a48
Tariq919 <97101939+Tariq919@users.noreply.github.com>
2024-07-31 11:37:33 +0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    document.getElementById('loginForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        database.ref('users').orderByChild('username').equalTo(username).once('value', snapshot => {
+            if (snapshot.exists()) {
+                const userData = Object.values(snapshot.val())[0];
+                if (userData.password === password) {
+                    localStorage.setItem('currentUser', username);
+                    window.location.href = 'main.html';
+                } else {
+                    document.getElementById('loginError').innerText = 'اسم المستخدم أو كلمة المرور غير صحيحة';
+                }
+            } else {
+                document.getElementById('loginError').innerText = 'اسم المستخدم أو كلمة المرور غير صحيحة';
+            }
+        });
+    });
+};
